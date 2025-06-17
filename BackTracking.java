4c245ec560a398d725f558d4e3786b70bbf8bdb1
Ambavaram Ameen <ambavaramameen@gmail.com>
2025-06-17 17:36:04 +0530

    Add files via upload

---
+++ BackTracking.java
@@ -0,0 +1,19 @@
+package java_with_DSA;
+
+public class BackTracking {
+	public static void makewords(String current,String letters) {
+		if(current.length() == 3) {
+			System.out.println(current);
+			return;
+		}
+		for(int i=0;i<letters.length();i++) {
+			makewords(current + letters.charAt(i),letters);
+		}
+	}
+	
+	public static void main(String[] args) {
+		
+		makewords(" ","ABC");
+	}
+
+}
